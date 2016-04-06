5aaba79fae76a3854ae3025e3011fc763cef17cb
WeRockStar <vr_freestyle@outlook.com>
2016-04-06 13:45:36 +0700

    Added retrofit and gson

--- app/src/main/java/com/werockstar/mvpgithubapi/view/MainActivity.java
+++ app/src/main/java/com/werockstar/mvpgithubapi/view/MainActivity.java
@@ -1,7 +1,9 @@
-package com.werockstar.mvpgithubapi;
+package com.werockstar.mvpgithubapi.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.werockstar.mvpgithubapi.R;
 
 public class MainActivity extends AppCompatActivity {
 
