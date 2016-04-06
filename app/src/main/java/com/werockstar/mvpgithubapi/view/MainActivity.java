6cb3fbba25e3659a106fca31d8bac381e4b66fd9
WeRockStar <vr_freestyle@outlook.com>
2016-04-06 14:08:32 +0700

    Added bind views

--- app/src/main/java/com/werockstar/mvpgithubapi/view/MainActivity.java
+++ app/src/main/java/com/werockstar/mvpgithubapi/view/MainActivity.java
@@ -2,14 +2,38 @@ package com.werockstar.mvpgithubapi.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.werockstar.mvpgithubapi.R;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.edtUsername)
+    private EditText edtUsername;
+
+    @Bind(R.id.btnLoad)
+    private Button btnLoad;
+
+    @Bind(R.id.imgProfile)
+    private ImageView imgProfile;
+
+    @Bind(R.id.tvFullName)
+    private TextView tvFullName;
+
+    @Bind(R.id.tvUsername)
+    private TextView tvUsername;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
     }
 }
