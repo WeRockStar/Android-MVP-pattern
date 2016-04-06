ea1c225a7f6af4f28ae147ed8314c680d44ab738
WeRockStar <vr_freestyle@outlook.com>
2016-04-06 14:05:17 +0700

    Added layout design

---
+++ app/src/main/java/com/werockstar/mvpgithubapi/custom/CustomTextView.java
@@ -0,0 +1,40 @@
+package com.werockstar.mvpgithubapi.custom;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.graphics.Typeface;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+public class CustomTextView extends TextView {
+    public CustomTextView(Context context) {
+        super(context);
+
+        init(context);
+    }
+
+    public CustomTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        init(context);
+    }
+
+    public CustomTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        init(context);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public CustomTextView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+
+        init(context);
+    }
+
+    public void init(Context context) {
+        Typeface tf = Typeface.createFromAsset(context.getAssets(), "fonts/thaisans.ttf");
+        setTypeface(tf);
+    }
+}
