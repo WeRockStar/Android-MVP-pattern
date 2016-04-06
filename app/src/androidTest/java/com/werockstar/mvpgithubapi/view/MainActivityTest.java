e096aaf2390a962e20f2196f0d0d749f2811d7d5
WeRockStar <vr_freestyle@outlook.com>
2016-04-06 15:29:04 +0700

    Added unit test and ui automate test

---
+++ app/src/androidTest/java/com/werockstar/mvpgithubapi/view/MainActivityTest.java
@@ -0,0 +1,38 @@
+package com.werockstar.mvpgithubapi.view;
+
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.IdlingResource;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.werockstar.mvpgithubapi.R;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+    @Rule
+    public ActivityTestRule<MainActivity> activity =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void put_github_username_should_show_fullname() {
+        onView(withId(R.id.edtUsername))
+                .perform(typeText("werockstar"), closeSoftKeyboard());
+        onView(withId(R.id.btnLoad))
+                .perform(click());
+
+        onView(withId(R.id.tvFullName))
+                .check(matches(withText("Kotchaphan Muangsan")));
+    }
+}
---
+++ app/src/test/java/com/werockstar/mvpgithubapi/presenter/GithubPresenterTest.java
@@ -0,0 +1,7 @@
+package com.werockstar.mvpgithubapi.presenter;
+
+import static org.junit.Assert.*;
+
+public class GithubPresenterTest {
+
+}
